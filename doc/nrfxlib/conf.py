4a845c8b603624d160f3095ad917e3927af9dacc
Farès Chati <fares.chati@intellinium.com>
2021-09-17 10:35:43 +0200

    Modified nRF's doc generation to include Intellinium's documentation
    This also modifies the theme of the documentation to add the Intellinium logo and give flexibility for future modifications.
    
    This should not cause too much conflict with future documentation updates from Nordic. In case a new documentation is added by Nordic, its theme should be updated to remain consistent with the rest. For that, just follow the modifications on:
        * nrf/doc/[the_new_doc]/conf.py
        * nrf/doc/CMakeLists.txt (just add the ITL_LIB_BASE environment variable to the python execution)
    
    All the Intellinium documentation content is located under ${ITL_LIB_BASE}/doc

--- doc/nrfxlib/conf.py
+++ doc/nrfxlib/conf.py
@@ -23,6 +23,12 @@ if not NRFXLIB_BUILD:
     raise FileNotFoundError("NRFXLIB_BUILD not defined")
 NRFXLIB_BUILD = Path(NRFXLIB_BUILD)
 
+# -- Intellinium addition
+ITL_LIB_BASE = os.environ.get("ITL_LIB_BASE")
+if not ITL_LIB_BASE:
+    raise FileNotFoundError("ITL_LIB_BASE not defined")
+ITL_LIB_BASE = Path(ITL_LIB_BASE)
+
 sys.path.insert(0, str(NRF_BASE / "doc" / "_utils"))
 import utils
 
@@ -53,7 +59,12 @@ rst_epilog = """
 
 # Options for HTML output ------------------------------------------------------
 
-html_theme = "sphinx_ncs_theme"
+# -- Intellinium modification
+# html_theme = "sphinx_ncs_theme"
+html_theme = "sphinx_itl_theme"
+# -- Intellinium addition
+html_theme_path = [str(ITL_LIB_BASE / "doc" / "_themes")]
+
 html_static_path = [str(NRF_BASE / "doc" / "_static")]
 html_last_updated_fmt = "%b %d, %Y"
 html_show_sourcelink = True
